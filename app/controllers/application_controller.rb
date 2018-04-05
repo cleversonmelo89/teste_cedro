8f8cfa89b62744fc7888d802cd6af8172de8d913
cleverson <kcm_melo@hotmail.com>
2018-04-04 23:50:59 -0300

    fix problem with cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+
+  after_action :set_cors
+
+  def set_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
